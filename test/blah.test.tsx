29bba167dac5c019fbe1f066fab62bd8a8232caa
ShankyTiwari <shanshank69@gmail.com>
2020-11-11 12:19:44 +0530

    test: Added initial test case

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,11 +1,22 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../src';
+
+import { MultilevelMenu } from '../src';
+import { list, configurations } from './../example/constants';
 
 describe('it', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
+    ReactDOM.render(
+      <MultilevelMenu
+        list={list}
+        configuration={configurations}
+        selectedListItem={event => {
+          console.log(event);
+        }}
+      />,
+      div
+    );
     ReactDOM.unmountComponentAtNode(div);
   });
 });
